2e3c7d2d8c341a50bec68df01eef9b8c66e572b6
Santiago Andres Guzman Fuentes <115915644+Santigfa@users.noreply.github.com>
2023-10-05 23:49:30 -0500

    Add files via upload

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../hojas-de-estilos/Testimonio.css';
+
+function Testimonio(props) {
+    return (
+      <div className='contenedor-testimonio'>
+        <img 
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt='Foto de emma' />
+        <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>
+           <strong>{props.nombre}</strong>  en {props.pais}
+            </p>
+
+          <p className='cargo-testimonio'>
+            {props.cargo} en <strong>{props.empresa}</strong>
+            </p>
+
+          <p className='texto-testimonio'>{props.testimonio}</p>
+
+        </div>
+      </div>
+    );
+}
+
+
+export default Testimonio;
